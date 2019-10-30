0931dc316644892ad700a468d7996b650656043b
gpiva <guilhermepiva@hotmail.com>
2019-10-30 10:39:04 -0300

    Aula 8 - Data Service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductModel } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  url = 'htttp://localhost:7189/v1';
+  
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<ProductModel[]> {
+    return this.http.get<ProductModel[]>(`${this.url}/products`);
+  }
+}
